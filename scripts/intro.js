507e0006723edba16b5223a013c33e185f769d18
GuiText <guilhermesilva028@hotmail.com>
2022-09-26 20:53:50 -0300

    first commit

---
+++ scripts/intro.js
@@ -0,0 +1,99 @@
+//document.write("Hello")
+
+//var myvar = "He"
+//const myconst = "He"
+
+//document.write(myconst + myvar)
+
+//string
+
+// "Isso é uma sring"
+// 'Isso também é uma string'
+// `Isso é uma string`
+
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+// document.write(bTrue)
+
+//objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: false,
+//   correr(){
+//     return "run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+ 
+
+// Funções - Funcionalidades - Atalhos - Reuso de códigos
+
+//registrar função
+// function sayMyName() {
+//   document.write("Gui")
+// }
+
+
+// executar
+// sayMyName()
+
+
+// Outra maneira de excutar
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName("Gui")
+// sayMyName("Danilo")
+// sayMyName("Ni")
+
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ) {
+//   //caminho 1
+//   document.write("Reprovado")
+// } else {
+//   //caminho 2
+//   document.write("Aprovado")
+// }
+
+
+// loop - Repetiições
+
+// for (i=0; i < 10; i++) {
+//   document.write("<p> alo </p>")
+// }
+
+
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
+
+
+ 
+   
+
+
+
+
+
+
